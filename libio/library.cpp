00a26c9528378505d76eea14a524251c9e88a2d0
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-07-23 14:32:22 +0800

    Added some method in IO

--- libio/library.cpp
+++ libio/library.cpp
@@ -14,7 +14,8 @@ extern "C" XScript::NativeClassInformation Initialize() {
 
 void println(XScript::ParamToMethod Param) {
     using namespace XScript;
-    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer), *static_cast<GarbageCollection *>(Param.VMGC));
+    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer),
+                                    *static_cast<GarbageCollection *>(Param.VMGC));
 
     EnvironmentStackItem Item = static_cast<Environment *>(Param.VMPointer)->Stack.PopValueFromStack();
     auto StringToPrint = static_cast<Environment *>(Param.VMPointer)->Heap.HeapData[
@@ -24,5 +25,58 @@ void println(XScript::ParamToMethod Param) {
     XBytes Str{wstring2string(&StringToPrint->Dest)};
     printf("%s\n", Str.c_str());
 
+    Interpreter.InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XIndexType) {}});
+}
+
+void readInt(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer),
+                                    *static_cast<GarbageCollection *>(Param.VMGC));
+
+    XInteger Int;
+    std::cin >> Int;
+    Interpreter.InterpreterEnvironment.Stack.PushValueToStack({EnvironmentStackItem::ItemKind::Integer,
+                                                               (EnvironmentStackItem::ItemValue) Int});
+    Interpreter.InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XIndexType) {}});
+}
+
+void readBool(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer),
+                                    *static_cast<GarbageCollection *>(Param.VMGC));
+
+    XBoolean Int;
+    std::cin >> Int;
+    Interpreter.InterpreterEnvironment.Stack.PushValueToStack({EnvironmentStackItem::ItemKind::Boolean,
+                                                               (EnvironmentStackItem::ItemValue) Int});
+    Interpreter.InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XIndexType) {}});
+}
+
+void readDeci(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer),
+                                    *static_cast<GarbageCollection *>(Param.VMGC));
+
+    XDecimal Deci;
+    std::cin >> Deci;
+    Interpreter.InterpreterEnvironment.Stack.PushValueToStack({EnvironmentStackItem::ItemKind::Decimal,
+                                                               (EnvironmentStackItem::ItemValue) Deci});
+    Interpreter.InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XIndexType) {}});
+}
+
+void readStr(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    BytecodeInterpreter Interpreter(*static_cast<Environment *>(Param.VMPointer),
+                                    *static_cast<GarbageCollection *>(Param.VMGC));
+
+    XBytes Str;
+    std::cin >> Str;
+    XHeapIndexType Idx = Interpreter.InterpreterEnvironment.Heap.PushElement(
+            (EnvObject) {XScript::EnvObject::ObjectKind::StringObject,
+                         (EnvObject::ObjectValue) CreateEnvStringObjectFromXString(string2wstring(Str))}
+    );
+
+    Interpreter.InterpreterEnvironment.Stack.PushValueToStack({EnvironmentStackItem::ItemKind::HeapPointer,
+                                                               (EnvironmentStackItem::ItemValue) Idx});
     Interpreter.InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XIndexType) {}});
 }
--- libio/library.h
+++ libio/library.h
@@ -9,6 +9,14 @@ void FuncReturn(XScript::Environment *Env);
 
 void println(XScript::ParamToMethod Param);
 
+void readInt(XScript::ParamToMethod Param);
+
+void readBool(XScript::ParamToMethod Param);
+
+void readDeci(XScript::ParamToMethod Param);
+
+void readStr(XScript::ParamToMethod Param);
+
 extern "C" XScript::NativeClassInformation Initialize();
 
 #endif //XSCRIPT2STDLIB_LIBRARY_H
