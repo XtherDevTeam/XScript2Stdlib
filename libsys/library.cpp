257d66f1ed68c071230423f2c74cc4b8a366a116
xiaokang00010 <zhouyongkang4321@gmail.com>
2022-08-01 20:38:46 +0800

    Added libsys

---
+++ libsys/library.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "library.h"
+
+extern "C" XScript::NativeLibraryInformation Initialize() {
+    XScript::XMap<XScript::XIndexType, XScript::NativeMethodInformation> Methods;
+    Methods[XScript::Hash(L"time_ms")] = {0, sys_time_ms};
+    Methods[XScript::Hash(L"exit")] = {0, sys_exit};
+
+    XScript::XMap<XScript::XIndexType, XScript::NativeClassInformation> Classes;
+    Classes[XScript::Hash(L"System")] = {L"System", Methods};
+    return {
+            L"Jerry Chou",
+            L"XScript 2 LibSystem",
+            Classes};
+}
+
+void sys_time_ms(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    auto Interpreter = static_cast<BytecodeInterpreter*>(Param.InterpreterPointer);
+    Interpreter->InterpreterEnvironment->Threads[Interpreter->ThreadID].Stack.PushValueToStack({
+        EnvironmentStackItem::ItemKind::Integer,
+        (EnvironmentStackItem::ItemValue)
+                static_cast<XInteger>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now().time_since_epoch()).count())
+    });
+    Interpreter->InstructionFuncReturn((BytecodeStructure::InstructionParam) {(XInteger) {}});
+}
+
+void sys_exit(XScript::ParamToMethod Param) {
+    using namespace XScript;
+    auto Interpreter = static_cast<BytecodeInterpreter*>(Param.InterpreterPointer);
+    EnvironmentStackItem Code = Interpreter->InterpreterEnvironment->Threads[Interpreter->ThreadID].Stack.PopValueFromStack();
+    Interpreter->InterpreterEnvironment->Heap.~EnvironmentHeap();
+    exit(Code.Value.IntVal);
+}
